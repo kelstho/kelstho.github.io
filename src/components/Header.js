ba86cba9d7f8ddc06c0143fbeba08d3594170bd0
Kelly Thompson <ksthompson48@gmail.com>
2020-05-07 13:27:15 -0500

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,34 @@
 import React from "react";
 
-function Header () {
+function Header() {
   return (
-    <h3>Kelly Thompson</h3>
+    <div className="container">
+      <div className="row">
+        <h3>Kelly Thompson</h3>
+      </div>
+      <div className="row">
+        <div className="col s12 m4">
+          <h6>email: <a href="mailto:ksthompson48@gmail.com">ksthompson48@gmail.com</a></h6>
+        </div>
+        <div className="col s12 m4">
+          <h5>-Web Developer-</h5>
+        </div>
+        <div className="col s12 m4">
+          <h6>phone: (785) 550-9520</h6>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col s12 m4">
+          <a href="https://docs.google.com/document/d/1q4bJ3po9KbRHmBZJxJrsRiOTP0jrL9nvRnGOvVOeS50/edit?usp=sharing" target="_blank" rel="noreferrer noopener">Resume</a>
+        </div>
+        <div className="col s12 m4">
+          <a href="https://github.com/kelstho" target="_blank" rel="noreferrer noopener">Github</a>
+        </div>
+        <div className="col s12 m4">
+          <a href="https://www.linkedin.com/in/kelly-thompson-5424541a1/" target="_blank" rel="noreferrer noopener">LinkedIn</a>
+        </div>
+      </div>
+    </div>
   );
 }
 
