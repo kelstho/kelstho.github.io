88f09d06b2a30118380328fc2ac0ff82dc9f964f
Kelly Thompson <ksthompson48@gmail.com>
2020-05-26 12:30:35 -0500

    update bio

--- src/pages/Bio.js
+++ src/pages/Bio.js
@@ -14,7 +14,7 @@ function Bio() {
             Hello there! My name is Kelly Thompson, and I am a former barista living in Waldo in Kansas City, Missouri. I’ve lived here for about three years now, and I’m really excited to be learning about coding from the University of Kansas! I love to travel as much as I can (preferably by car, if possible). I’ve been to 37 states (including Alaska) but as far as visiting other countries, I’ve only ever been to Canada. I'm hoping to expand that list soon though!
         </p>
           <p>
-            I was born and raised in Lawrence, Kansas. I graduated from Lawrence High School in 2011, and I received a BFA in Photography from the Lesley University College of Art and Design in 2016. Since then, I have been working in the coffee industry as a barista. Coffee is one of my greatest passions, but I’m ready for a major career change and am hoping to be hired as a web developer after my graduation from this boot camp.
+            I was born and raised in Lawrence, Kansas. I graduated from Lawrence High School in 2011, and I received a BFA in Photography from the Lesley University College of Art and Design in 2016. I worked in the coffee industry as a barista for my first couple of years here in KC, and am now hoping to secure a job as a Front End Developer. Please feel free to reach out via phone or email to contact me with any questions about my work!
         </p>
         </div>
       </div>
